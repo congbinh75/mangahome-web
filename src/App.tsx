21d2f2d7ce6317529fe26d2d1a289646c35a2da2
congbinh75 <dangcongbinh36@gmail.com>
2025-04-05 19:46:06 +0700

    Add simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { Outlet } from "react-router";
+import { ThemeProvider } from "./components/theme-provider";
+import { Layout } from "./components/layout";
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <Layout>
+        <Outlet />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default App;
